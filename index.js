d2ab74f5ea54f40b3327094ed29cf649d9365bba
maoya12345 <jannatulmaoya456@gmail.com>
2026-06-11 02:51:49 +0600

    mediqueue server port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 require('dotenv').config()
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.DB_URI;
